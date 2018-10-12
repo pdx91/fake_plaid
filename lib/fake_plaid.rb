bf005056ea4451eaadf5f845e487df4a2c341c48
Pradyumna <pradyumna2905@gmail.com>
2018-10-12 15:50:34 -0700

    Wireframe.

--- lib/fake_plaid.rb
+++ lib/fake_plaid.rb
@@ -1,5 +1,13 @@
-require "fake_plaid/version"
+require 'fake_plaid/initializers/webmock'
+require 'fake_plaid/stub_app'
 
 module FakePlaid
-  # Your code goes here...
+  def self.stub_plaid
+    Plaid.env = 'sandbox'
+    Plaid.client_id = 'PLAID_CLIENT_ID'
+    Plaid.secret = 'PLAID_SECRET'
+    Plaid.public_key = 'PLAID_PUBLIC_KEY'
+
+    stub_request(:any, /sandbox.plaid.com/).to_rack(FakePlaid::StubApp)
+  end
 end
---
+++ lib/fake_plaid/stub_app.rb
@@ -0,0 +1,7 @@
+require 'sinatra/base'
+
+module FakePlaid
+  class StubApp < Sinatra::Base
+    # Exchange token response
+  end
+end
