0ce3c192ac7eab99e360faad82f2b1406c3e889a
Pradyumna <pradyumna2905@gmail.com>
2018-10-14 18:46:46 -0700

    Fixtures for creating an item.
    - This is the first post request made to Plaid via the link.

--- lib/fake_plaid/stub_app.rb
+++ lib/fake_plaid/stub_app.rb
@@ -2,6 +2,23 @@ require 'sinatra/base'
 
 module FakePlaid
   class StubApp < Sinatra::Base
-    # Exchange token response
+    # Create an Item
+    post '/link/item/create' do
+      if sandbox_credentials_valid?(params)
+        json_response 200, fixture('create_item')
+      end
+    end
+
+    private
+
+    def sandbox_credentials_valid?(params)
+      params.dig('credentials', 'username') == 'user_good' &&
+        params.dig('credentials', 'password') == 'pass_good'
+    end
+
+    def fixture(file_name)
+      file_path = File.join(FakePlaid.fixture_path, "#{file_name}.json")
+      File.open(file_path, 'rb').read
+    end
   end
 end
