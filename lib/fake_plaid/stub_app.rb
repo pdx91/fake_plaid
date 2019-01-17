fd04405645727cdc8564487f248c31d20f63f82d
Pradyumna Shembekar <pradyumna2905@gmail.com>
2019-01-16 19:18:27 -0800

    Support for transactions/get params.

--- lib/fake_plaid/stub_app.rb
+++ lib/fake_plaid/stub_app.rb
@@ -16,19 +16,12 @@ module FakePlaid
     end
 
     post '/transactions/get' do
-      json_response 200, OpenStruct.new(transactions: fixture('transactions'))
+      transactions = derive_transactions_from_params
+
+      json_response 200, transactions
     end
 
     private
-
-    # Monkey-paching `params` method.
-    #
-    # Requests from Plaid come in but the `params` are not populated.
-    def _params
-      return params if params.present?
-
-      @_params ||= JSON.parse(request.body.read)
-    end
 
     def sandbox_credentials_valid?
       params.dig('credentials', 'username') == 'user_good' &&
@@ -44,6 +37,54 @@ module FakePlaid
     def fixture(file_name)
       file_path = File.join(FakePlaid.fixture_path, "#{file_name}.json")
       File.open(file_path, 'rb').read
+    end
+
+    # Monkey-paching `params` method.
+    #
+    # Requests from Plaid come in but the `params` are not populated.
+    def _params
+      return params if params.present?
+
+      @_params ||= JSON.parse(request.body.read)
+    end
+
+    def derive_transactions_from_params
+      if valid_transaction_params?
+        transactions_json = JSON.parse(fixture('transactions')).dig('transactions')
+
+        transactions = transactions_json.select do |txn|
+          txn.dig('date') > start_date && txn.dig('date') < end_date
+        end
+
+        JSON.generate(
+          'transactions' =>
+          if transaction_count && transaction_count > 0
+            transactions[0..(transaction_count - 1)]
+          else
+            transactions
+          end
+        )
+      end
+    end
+
+    def valid_transaction_params?
+      _params.dig('access_token') &&
+        _params.dig('secret') &&
+        _params.dig('client_id') &&
+        _params.dig('start_date') &&
+        _params.dig('end_date')
+    end
+
+    def start_date
+      _params.dig('start_date')
+    end
+
+    def end_date
+      _params.dig('end_date')
+    end
+
+    def transaction_count
+      _params.dig('options', 'count')
     end
   end
 end
